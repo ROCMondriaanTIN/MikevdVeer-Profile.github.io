9378d47c4977b73413b6b55276ad555bbb46a252
MikevdVeer <302815715@student.rocmondriaan.nl>
2024-03-09 20:33:15 +0100

    Not done yet

--- Js/main.js
+++ Js/main.js
@@ -1,12 +1,3 @@
-// const submitBtn = document.querySelector(".submit-btn");
-// submitBtn.addEventListener("click", function() {
-//     const validRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-//     const firstName = document.querySelector(".form-name");
-//     if (email.value.match(validRegex)) {
-//         alert("valid email");
-//         window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ", "_blank")
-//     }
-// })
 
 document.addEventListener('DOMContentLoaded', function () {
   const menuIcon = document.querySelector('.menu-icon');
