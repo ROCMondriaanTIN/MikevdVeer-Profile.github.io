a715f8db9ed4dda27a37bd66bec3b1bdb1981c16
MikevdVeer <302815715@student.rocmondriaan.nl>
2024-03-16 15:22:42 +0100

    Profile remake v3

--- Js/main.js
+++ Js/main.js
@@ -16,6 +16,16 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
